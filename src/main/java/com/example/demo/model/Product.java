1bf8345e988b8a72dd8d43e85e19c5742e9edeeb
NGUYEN NGOC SON <sonvipkl04@gmail.com>
2025-10-24 09:25:58 +0700

    .

---
+++ src/main/java/com/example/demo/model/Product.java
@@ -0,0 +1,16 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private double price;
+    private int description;
+
+}
